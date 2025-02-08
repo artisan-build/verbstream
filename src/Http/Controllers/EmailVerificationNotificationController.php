5c518de359e3566bc63f23138930f8f673479dc4
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-08 22:13:30 +0100

    Automated code quality

--- src/Http/Controllers/EmailVerificationNotificationController.php
+++ src/Http/Controllers/EmailVerificationNotificationController.php
@@ -3,19 +3,17 @@
 namespace ArtisanBuild\Verbstream\Http\Controllers;
 
 use ArtisanBuild\Verbstream\Events\EmailVerificationNotificationSent;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Laravel\Fortify\Contracts\EmailVerificationNotificationSentResponse;
 use Laravel\Fortify\Http\Responses\RedirectAsIntended;
-use Illuminate\Contracts\Support\Responsable;
 
 class EmailVerificationNotificationController extends Controller
 {
     /**
      * Send a new email verification notification.
-     *
-     * @return JsonResponse|Responsable
      */
     public function store(Request $request): JsonResponse|Responsable
     {
