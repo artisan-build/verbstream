ef1f514583cddbdbab060478502f701627a36155
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-12 13:36:19 +0100

    Fix a missing blade component to allow optimization in production

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -12,7 +12,6 @@
             @livewire('teams.team-member-manager', ['team' => $team])
 
             @if (Gate::check('delete', $team) && ! $team->personal_team)
-                <x-section-border />
 
                 <div class="mt-10 sm:mt-0">
                     @livewire('teams.delete-team-form', ['team' => $team])
