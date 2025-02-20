d4491a8a4dce7d0a3c4a21113cefc300d0398620
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-20 16:23:40 +0100

    Up to Tailwind 4 and Flux 2

--- resources/views/api/api-token-manager.blade.php
+++ resources/views/api/api-token-manager.blade.php
@@ -108,7 +108,7 @@
             </div>
 
             <flux:input x-ref="plaintextToken" type="text" readonly :value="$plainTextToken"
-                     class="mt-4 bg-gray-100 px-4 py-2 rounded font-mono text-sm text-gray-500 w-full break-all"
+                     class="mt-4 bg-gray-100 px-4 py-2 rounded-sm font-mono text-sm text-gray-500 w-full break-all"
                      autofocus autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
                      @showing-token-modal.window="setTimeout(() => $refs.plaintextToken.select(), 250)"
             />
